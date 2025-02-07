3694e3e20afb20c95105b4d55eca185763e86d9c
Aryan Shrivastava <aryan.shrivastava@kuliza.com>
2025-02-08 00:49:26 +0530

    setup commit

---
+++ src/main/java/com/learning_platform/lectureMgmt/LectureMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.learning_platform.lectureMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning_platform/lectureMgmt/LectureMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning_platform.lectureMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
